6f52b846cfa272569d2f78474103181b179b2187
vinig28 <g.vinisha2001@gmail.com>
2024-06-24 11:52:23 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'little_lemon_db',
+        'USER': 'your_mysql_user',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
